417602ddfd2cbea34a1b6afb956b5910a91cc8c7
Glitch (smooth-chip-diploma) <none>
2021-05-11 04:44:48 +0000

    🍢🏭 Checkpoint ./index.js:30748394/2355 ./package.json:30748394/10189

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const {Collection, Client, Discord} = require('discord.js')
-const keep_alive = require("./src/keep_alive.js")
-const { Database } = require("quickmongo")
-const fs = require('fs')
-require("dotenv").config
+const { Collection, Client, Discord } = require("discord.js");
+const keep_alive = require("./src/keep_alive.js");
+const { Database } = require("quickmongo");
+const fs = require("fs");
+require("dotenv").config();
 const client = new Client({
-    disableEveryone: true
-})
-module.exports = client
-const config = require('./config/config.json')
-const prefix = config.prefix
+  disableEveryone: true
+});
+module.exports = client;
+const config = require("./config/config.json");
+const prefix = config.prefix;
 const token = process.env.TOKEN;
 client.gg = config;
-client.db = new Database("mongodb+srv://ngg:CTRj4dXxUdsRRwJ@wax.vfa3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-client.emotes = require("./config/emojis.json")
+client.db = new Database(
+  "mongodb+srv://ngg:CTRj4dXxUdsRRwJ@wax.vfa3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+client.emotes = require("./config/emojis.json");
 client.commands = new Collection();
 client.aliases = new Collection();
 client.categories = fs.readdirSync("./commands/");
 ["command"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-}); 
-
+  require(`./handlers/${handler}`)(client);
+});
 
 const { GiveawaysManager } = require("discord-giveaways");
 client.giveawaysManager = new GiveawaysManager(client, {
@@ -33,6 +34,6 @@ client.giveawaysManager = new GiveawaysManager(client, {
 });
 
 client.db.on("ready", () => {
-	console.log("Connected with mongodb Database!")
-})
-client.login(token)
+  console.log("Connected with mongodb Database!");
+});
+client.login(token);
