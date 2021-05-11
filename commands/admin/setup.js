b37b1b3eeeb0cdb3404eb920b2c504843b8979dd
Glitch (smooth-chip-diploma) <none>
2021-05-11 11:37:01 +0000

    🏘👹 Checkpoint ./commands/admin/setup.js:30748394/315

--- commands/admin/setup.js
+++ commands/admin/setup.js
@@ -5,15 +5,19 @@ module.exports = {
 	description: "Setup server config",
 	useage: "",
 	run: (client, message, args) => {
+    if (!message.author.hasPermission("ADMINISTRATOR")) return message.reply("You Have not permission to use this command")
     let choice = args[0]
     
     const mainembed = new MessageEmbed()
     .setColor("RED")
     .setTitle("❗NO CHOICE SELECTED")
+    .setDescription("Please select which section you want to setup!")
     .addField("🔑 USEAGE", `x!setup <section name> [value]`)
     .addField(`\u200B`, `__GENERAL__`)
-    .addField("👋 Welcome channel", `Section Name: **welcomeChannel**`)
-    .addField(`🚶goodbye channel`, `Section Name: **goodbye Channel**`)
+    .addField("👋 Welcome channel", `Section Name : **\`welcomeChannel**\``)
+    .addField(`🚶goodbye channel`, `Section Name : **\`goodbye Channel\`**`)
+    .addField("
+    
 
     
     
