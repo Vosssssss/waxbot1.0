745d248294ca04ddc601c591164208f6e5aa3f21
Glitch (smooth-chip-diploma) <none>
2021-05-12 16:49:06 +0000

    🍝🚐 Checkpoint ./package.json:30748394/4122 ./nesbroDB.js:30748394/3

--- nesbroDB.js
+++ nesbroDB.js
@@ -2,7 +2,7 @@ const { nesbroDB } = require("nesbros");
 
 const ndb = new nesbroDB({
 
-    uri: "mongodb+srv://ngg:CTRj4dXxUdsRRwJ@wax.vfa3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    uri: "mongodb+srv://ngg:CTRj4dXxUdsRRwJ@wax.vfa3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 
 });
 
