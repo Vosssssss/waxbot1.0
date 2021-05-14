380e783be2e35f74292e165c4d007a56cb099cb0
Glitch (smooth-chip-diploma) <none>
2021-05-14 05:07:44 +0000

    🚍⏱ Checkpoint ./events/guildMemberAdd.js:30748394/2440 ./package.json:30748394/5992

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,23 +1,36 @@
-const client = require("../index")
-const Discord = require("discord.js")
-const db = client.db;
-const { CanvasSenpai } = require('canvas-senpai');
-const canva = new CanvasSenpai();
+const client = require('../index');
+const canvas = require('discord-canvas')
+const { MessageAttachment } = require('discord.js')
+const Discord = require('discord.js')
 
-/**
- * @param {Object} [client]
- * @param {Object} [member]
- */
+client.on('guildMemberAdd', async(member) => {
+  
+  const db = client.db;
+  let channel = await db.fetch(`welcome-${member.guild.id}`)
+  let chx = await db.get(`welcome-${member.guild.id}`)
+  
+        const user = member.user;
+        const image = await new canvas.Welcome()
+        .setUsername(user.username)
+        .setDiscriminator(user.discriminator)
+        .setMemberCount(member.guild.memberCount)
+        .setGuildName(member.guild.name)
+        .setAvatar(user.displayAvatarURL({ format: "png" }))
+        .setColor("border", "#8015EA")
+        .setColor("username-box", "#8015EA")
+        .setColor("discriminator-box", "#8015EA")
+        .setColor("message-box", "#8015EA")
+        .setColor("title", "#8015EA")
+        .setColor("avatar", "#8015EA")
+        .setBackground("https://st2.depositphotos.com/1265894/10726/i/950/depositphotos_107269762-stock-photo-winter-ice-frost-frozen-background.jpg")
+        .toAttachment();
+ if (channel){
+        const attachment = new Discord.MessageAttachment(
+            (await image).toBuffer(),
+             "welcome-image.png"
+             );
 
-client.on('guildMemberLeave', async member => {
-	let msg = `Bye bye <@${member.id}`
-	const ChannelID = db.get(`leave-${member.guild.id}`);
-	if (!ChannelID) return;
-	let data = await canva.welcome(member, {
-		link:
-			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB_KPNodipmtrMQ62SJ28NXrnOUUhzekEBQg&usqp=CAU'
-	});
-	client.channels.cache
-		.get(ChannelID)
-		.send(msg, new Discord.MessageAttachment(data, 'welcome-image.png'));
-});
+             const channel = member.guild.channels.cache.get(chx);
+             channel.send(attachment)
+   }
+    })
