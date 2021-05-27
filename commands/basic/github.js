92a5a71ed562c465616a235cc0ea771b9a5fe80d
Glitch (smooth-chip-diploma) <none>
2021-05-27 06:37:43 +0000

    🍬👀 Checkpoint ./index.js:30748394/60 ./commands/basic/github.js:30748394/174

--- commands/basic/github.js
+++ commands/basic/github.js
@@ -14,14 +14,17 @@ module.exports = {
 		if (!name) {
 			return message.channel.send(` Please provide a valid user`);
 		}
-
+    
+    let errorMsg = client.error
+    
+    
 		const url = `https://api.github.com/users/${name}`;
 
 		let response;
 		try {
 			response = await fetch(url).then(res => res.json());
 		} catch (e) {
-			return message.channel.send(`An error occured, please try again!`);
+			return errorMsg(`An error occured, please try again!`, message.channel);
 		}
 
 		try {
@@ -66,7 +69,7 @@ module.exports = {
 
 			message.channel.send(embed);
 		} catch (err) {
-			return message.channel.send(` Please provide a valid user`);
+			return errorMsg(` Please provide a valid user`, message.channel);
 		}
 	}
 };
